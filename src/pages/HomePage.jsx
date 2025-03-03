df47dcb4ba9823c9714bbb6e889d413424fc4f3b
arafatah <arafathowlader890@gmail.com>
2025-03-03 13:20:10 +0600

    add PageNav component and integrate it into Home, Pricing, Product, and PageNotFound pages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import PageNav from "../Components/PageNav";
 
 function HomePage() {
   return (
     <div>
+      <PageNav />
       <h1>HomePage</h1>
       <Link to="/pricing">Go to Price</Link>
     </div>
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,12 @@
+import PageNav from "../Components/PageNav";
+
 function PageNotFound() {
-  return <div>Page not found</div>;
+  return (
+    <div>
+      <PageNav />
+      Page not found
+    </div>
+  );
 }
 
 export default PageNotFound;
